f5293814a0868e33fa7fea4210211354a46f92a6
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-10-30 12:47:35 +0530

    footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,24 @@
 import classes from "./footer.module.css";
+import FaceBook from "@mui/icons-material/Facebook";
+import Google from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
     <div className={classes.dark}>
-      <div className={classes.section}>
-        <p>This is the footer</p>
-        @sl app-xperts || all right reserved
+      <div className={classes.contact_us}>
+        <h3>Contact Us</h3>
+        <hr />
+
+        <div className={classes.contact__medias}>
+          <FaceBook />
+          <a href="mailto:dilshanthilina53@gmail.com" target="_blank">
+            <Google />
+          </a>
+        </div>
+        <hr />
+      </div>
+      <div className={classes.copy_right}>
+        © sl app-xperts || all right reserved
       </div>
     </div>
   );
